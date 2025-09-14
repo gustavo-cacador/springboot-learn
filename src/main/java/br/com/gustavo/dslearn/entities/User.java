a0c79e79f7f66642dd6d61f16b07bad1d73273b7
Gustavo <gustavoirl456@outlook.com>
2025-09-14 17:56:05 -0300

    implementando entidade Topic

--- src/main/java/br/com/gustavo/dslearn/entities/User.java
+++ src/main/java/br/com/gustavo/dslearn/entities/User.java
@@ -24,6 +24,9 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<Notification> notifications = new ArrayList<>();
 
+    @OneToMany(mappedBy = "author")
+    private List<Topic> topics = new ArrayList<>();
+
     public User() {
     }
 
@@ -73,6 +76,10 @@ public class User {
 
     public List<Notification> getNotifications() {
         return notifications;
+    }
+
+    public List<Topic> getTopics() {
+        return topics;
     }
 
     @Override
