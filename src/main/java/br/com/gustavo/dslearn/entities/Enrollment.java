16dacbd9f67179b2d5edda0c90cb1ad8b3220e0a
Gustavo <gustavoirl456@outlook.com>
2025-09-14 00:36:37 -0300

    implementando entidade Enrollment e EnrollmentPK

---
+++ src/main/java/br/com/gustavo/dslearn/entities/Enrollment.java
@@ -0,0 +1,84 @@
+package br.com.gustavo.dslearn.entities;
+
+import br.com.gustavo.dslearn.entities.pk.EnrollmentPK;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_enrollment")
+public class Enrollment {
+
+    @EmbeddedId
+    private EnrollmentPK id = new EnrollmentPK();
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant enrollMoment;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant refoundMoment;
+
+    // colocando boolean como tipo primitivo pois só queremos valor true ou false, se quisessemos true false ou null colocariamos como wrapper
+    private boolean available;
+    private boolean onlyUpdate;
+
+    public Enrollment() {
+    }
+
+    public Enrollment(User user, Offer offer, Instant enrollMoment, Instant refoundMoment, boolean available, boolean onlyUpdate) {
+        id.setUser(user);
+        id.setOffer(offer);
+        this.enrollMoment = enrollMoment;
+        this.refoundMoment = refoundMoment;
+        this.available = available;
+        this.onlyUpdate = onlyUpdate;
+    }
+
+    public User getStudent() {
+        return id.getUser();
+    }
+
+    public void setStudent(User user) {
+        id.setUser(user);
+    }
+
+    public Offer getOffer() {
+        return id.getOffer();
+    }
+
+    public void setOffer(Offer offer) {
+        id.setOffer(offer);
+    }
+
+    public Instant getEnrollMoment() {
+        return enrollMoment;
+    }
+
+    public void setEnrollMoment(Instant enrollMoment) {
+        this.enrollMoment = enrollMoment;
+    }
+
+    public Instant getRefoundMoment() {
+        return refoundMoment;
+    }
+
+    public void setRefoundMoment(Instant refoundMoment) {
+        this.refoundMoment = refoundMoment;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public boolean isOnlyUpdate() {
+        return onlyUpdate;
+    }
+
+    public void setOnlyUpdate(boolean onlyUpdate) {
+        this.onlyUpdate = onlyUpdate;
+    }
+}
